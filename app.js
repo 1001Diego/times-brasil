344574d29b9f37eb22f218805cbd74753421ebc0
1001Diego <diegogomesgontijo1001@gmail.com>
2024-09-17 18:23:25 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -2,17 +2,11 @@ function pesquisar() {
     // Função responsável por buscar dados e exibir os resultados
     // em uma seção HTML.
     
-    let section = document.getElementById("resultados-pesquisa");
+    let section = document.getElementById("resultados-pesquisa")
 
-    let campoPesquisa = document.getElementById("campoPesquisa").value
+    let campoPesquisa = document.getElementById("campo-pesquisa").value
 
-    // se campoPesquisa for uma string sem nada
-    if (!campoPesquisa) {
-      section.ineerHTML = "<p>Nada foi encontrado. Digite o nome do time</p>"
-      return
-    }
-
-    campoPesquisa = campoPesquisa.toLowerCase()
+    campoPesquisa = campoPesquisa.toLowerCase() 
 
     // Obtém a seção HTML com o ID "resultados-pesquisa" onde os resultados serão exibidos.
   
@@ -20,18 +14,25 @@ function pesquisar() {
     let titulo = "";
     let descricao = "";
     let tags = "";
+    let dataFundacao = "";
 
     // Inicializa uma string vazia para armazenar os resultados da busca.
     for (let dado of dados) {
       titulo = dado.titulo.toLowerCase()
       descricao = dado.descricao.toLowerCase()
+      dataFundacao =dado.dataFundacao.toLowerCase()
       tags = dado.tags.toLowerCase()
       // se titulo includes campoPesquisa.
       if (titulo.includes(campoPesquisa) || 
-    descricao.includes(campoPesquisa) || tags.includes(campoPesquisa)) {
+    descricao.includes(campoPesquisa) || dataFundacao.includes(campoPesquisa)  ||
+    tags.includes(campoPesquisa)) {
       //cria novo elemento para cada resultado
               resultados += `
         <div class="item-resultado">
+        <div class="imagem-resultado">
+                <img src="${dado.imagem}" class="foto" 
+                alt=Foto de"${dado.titulo}">
+            </div>
           <h2>
             <a href="#" target="_blank">${dado.titulo}</a>
           </h2>
@@ -40,11 +41,16 @@ function pesquisar() {
         </div> 
       `;
     }
-    }
-    if (!resultados) {
-      resultados = "<p>Nada foi encontrado</p>"
-    }
-
+      // se campoPesquisa for uma string sem nada
+      if (campoPesquisa == ""){
+        section.innerHTML = "<p> Nada foi encontrado, pesquise o time desejado! </p>" 
+        return
+      }
+      if (!section.innerHTML) {
+          section.innerHTML = "<p> Nada foi encontrado, pesquise o time desejado! </p>"
+          return
+        }
+}
       // Atribui o HTML gerado à seção, atualizando o conteúdo da página.
     section.innerHTML = resultados;
   }
--- dados.js
+++ dados.js
@@ -1,20 +1,34 @@
+//variavel, nome e objetos
 let dados = [
 {
     titulo : "Cruzeiro",
     descricao : "O Cruzeiro Esporte Clube é um gigante do futebol brasileiro, com uma torcida apaixonada e uma história vitoriosa. Fundado em Belo Horizonte, o clube conquistou diversos títulos nacionais e internacionais, como 4 Campeonatos Brasileiros, 6 Copas do Brasil (O maior vencedor), 2 Copas Libertadores da América e diversos outros. Sua rivalidade com o Atlético Mineiro é uma das maiores do país, e a Raposa, como é carinhosamente chamada, é um dos maiores símbolos do futebol mineiro.",
+    dataFundacao : "2 de janeiro de 1921",
+    imagem : "",
+    imgalt: "Imagem escudo do Cruzeiro",
+    instagram: "https://www.instagram.com/cruzeiro",
     link : "https://pt.wikipedia.org/wiki/Cruzeiro_Esporte_Clube",
-    tags : "raposa zeiro zero minas bh"
+    tags : "raposa  zeiro  zero  minas  bh  cabuloso",
+
 },
 {
     titulo : "Flamengo",
     descricao : "O Flamengo, o Mengão, é um dos clubes mais populares de todo o mundo. Com uma torcida apaixonada e uma história vitoriosa, o rubro-negro carioca é um dos maiores vencedores do futebol brasileiro, conquistando aproximadamente 8 títulos brasileiros, 4 Copas do Brasil, 3 Copas Libertadores da América e 1 Mundial de Clubes.",
+    dataFundacao : "17 de novembro de 1895",
+    imagem : "",
+    imgalt: "Imagem escudo do Flamengo",
+    instagram: "https://www.instagram.com/flamengo",
     link : "https://pt.wikipedia.org/wiki/Clube_de_Regatas_do_Flamengo",
-    tags : "mengo mengudo fla rio rj"
+    tags : "mengo  mengudo  fla  rio  rj  mengao",
 },
 {
     titulo : "São Paulo",
     descricao : "O São Paulo Futebol Clube, o Tricolor Paulista, é um dos clubes mais tradicionais do Brasil. Com uma história vitoriosa e uma torcida apaixonada, o clube já conquistou diversos títulos nacionais e internacionais, como 6 Campeonatos Brasileiros, 1 Copa do Brasil, 3 Copas Libertadores da América e 3 Mundiais de Clubes.",
+    dataFundacao :"25 de janeiro de 1930",
+    imagem : "",
+    imgalt: "Imagem escudo do São Paulo",
+    instagram: "https://www.instagram.com/saopaulofc",
     link : "https://pt.wikipedia.org/wiki/São_Paulo_Futebol_Clube",
-    tags : "sp trikas tricas sao paulo"
+    tags : "sp  trikas  tricas  sao paulo  tricolor",
 }
 ];
